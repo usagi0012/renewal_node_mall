adaf6adadfe6d2092fba149c607616fdd872d74e
jiwon <usagi0012@naver.com>
2023-12-09 21:52:35 +0900

    상품 API에 에러 미들웨어 적용/ 회원 정보 수정 API 구현

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -35,107 +35,123 @@ router.post('/products', authMiddleware, async (req, res) => {
 
 //상품 수정 API
 router.put('/products/:productId', authMiddleware, async (req, res) => {
-  const { productId } = req.params;
-  const userId = req.user.userId;
-  const { productName, productContent, productStatus } = req.body;
-  const updateProduct = await prisma.products.findFirst({ where: { productId: +productId } });
+  try {
+    const { productId } = req.params;
+    const userId = req.user.userId;
+    const { productName, productContent, productStatus } = req.body;
+    const updateProduct = await prisma.products.findFirst({ where: { productId: +productId } });
 
-  //상품이 있는지 조회
-  if (!updateProduct) {
-    return res.status(404).json({ Message: '상품 조회에 실패하였습니다.' });
+    //상품이 있는지 조회
+    if (!updateProduct) {
+      return res.status(404).json({ Message: '상품 조회에 실패하였습니다.' });
+    }
+
+    //있으면 본인이 등록한 상품인지 확인
+    if (updateProduct.userId !== userId) {
+      return res.status(401).json({ Message: '상품을 수정할 권한이 존재하지 않습니다.' });
+    }
+
+    //인증되었으면 상품 수정
+    await prisma.products.update({
+      where: { productId: +productId },
+      data: {
+        productName,
+        productContent,
+        productStatus,
+      },
+    });
+    res.status(200).json({ Message: '상품정보를 수정하였습니다.' });
+  } catch (err) {
+    next(err);
   }
-
-  //있으면 본인이 등록한 상품인지 확인
-  if (updateProduct.userId !== userId) {
-    return res.status(401).json({ Message: '상품을 수정할 권한이 존재하지 않습니다.' });
-  }
-
-  //인증되었으면 상품 수정
-  await prisma.products.update({
-    where: { productId: +productId },
-    data: {
-      productName,
-      productContent,
-      productStatus,
-    },
-  });
-  res.status(200).json({ Message: '상품정보를 수정하였습니다.' });
 });
 
 //상품 삭제 API
 router.delete('/products/:productId', authMiddleware, async (req, res) => {
-  const { productId } = req.params;
-  const userId = req.user.userId;
-  const deleteProduct = await prisma.products.findFirst({ where: { productId: +productId } });
+  try {
+    const { productId } = req.params;
+    const userId = req.user.userId;
+    const deleteProduct = await prisma.products.findFirst({ where: { productId: +productId } });
 
-  //상품이 있는지 조회
-  if (!deleteProduct) {
-    return res.status(404).json({ Message: '상품 조회에 실패하였습니다.' });
+    //상품이 있는지 조회
+    if (!deleteProduct) {
+      return res.status(404).json({ Message: '상품 조회에 실패하였습니다.' });
+    }
+
+    //있으면 본인이 등록한 상품인지 확인
+    if (deleteProduct.userId !== userId) {
+      return res.status(401).json({ Message: '상품을 삭제할 권한이 존재하지 않습니다.' });
+    }
+
+    //인증되었으면 상품 삭제
+    await prisma.products.delete({ where: { productId: +productId } });
+    res.status(200).json({ Message: '상품을 삭제하였습니다.' });
+  } catch (err) {
+    next(err);
   }
-
-  //있으면 본인이 등록한 상품인지 확인
-  if (deleteProduct.userId !== userId) {
-    return res.status(401).json({ Message: '상품을 삭제할 권한이 존재하지 않습니다.' });
-  }
-
-  //인증되었으면 상품 삭제
-  await prisma.products.delete({ where: { productId: +productId } });
-  res.status(200).json({ Message: '상품을 삭제하였습니다.' });
 });
 
 //상품 목록 조회 API
 router.get('/products//:sort', async (req, res) => {
-  const { sort } = req.params;
+  try {
+    const { sort } = req.params;
 
-  //asc면 과거순 정렬
-  if (sort == 'ASC') {
-    const products = await prisma.products.findMany({
-      select: {
-        productId: true,
-        productName: true,
-        productStatus: true,
-        userId: true,
-        createdAt: true,
-      },
-      orderBy: {
-        productId: 'asc',
-      },
-    });
-    return res.status(200).json({ data: products });
-  } else {
-    //나머지 경우에는 전부 최신순
-    const products = await prisma.products.findMany({
-      select: {
-        productId: true,
-        productName: true,
-        productStatus: true,
-        userId: true,
-        createdAt: true,
-      },
-      orderBy: {
-        productId: 'desc',
-      },
-    });
-    return res.status(200).json({ data: products });
+    //asc면 과거순 정렬
+    if (sort == 'ASC') {
+      const products = await prisma.products.findMany({
+        select: {
+          productId: true,
+          productName: true,
+          productStatus: true,
+          userId: true,
+          createdAt: true,
+        },
+        orderBy: {
+          productId: 'asc',
+        },
+      });
+      return res.status(200).json({ data: products });
+    } else {
+      //나머지 경우에는 전부 최신순
+      const products = await prisma.products.findMany({
+        select: {
+          productId: true,
+          productName: true,
+          productStatus: true,
+          userId: true,
+          createdAt: true,
+        },
+        orderBy: {
+          productId: 'desc',
+        },
+      });
+      return res.status(200).json({ data: products });
+    }
+  } catch (err) {
+    next(err);
   }
 });
 
 //상품 상세 조회 API
 router.get('/products/:productId', async (req, res) => {
-  const { productId } = req.params;
+  try {
+    const { productId } = req.params;
 
-  const detailProduct = await prisma.products.findFirst({
-    where: {
-      productId: +productId,
-    },
-  });
+    const detailProduct = await prisma.products.findFirst({
+      where: {
+        productId: +productId,
+      },
+    });
 
-  //상품이 없을때
-  if (!detailProduct) {
-    return res.status(404).json({ Message: '상품 조회에 실패하였습니다.' });
+    //상품이 없을때
+    if (!detailProduct) {
+      return res.status(404).json({ Message: '상품 조회에 실패하였습니다.' });
+    }
+
+    res.status(200).json({ data: detailProduct });
+  } catch (err) {
+    next(err);
   }
-
-  res.status(200).json({ data: detailProduct });
 });
 
 export default router;
